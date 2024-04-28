c71bb8657a5aba989bd706ac3b53ab0dfe17bfcd
HG <kthoangst1@gmail.com>
2024-04-28 20:05:01 +0700

    First Commit

---
+++ Winform/AppQuanLyQuanCafe/Login.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace AppQuanLyQuanCafe
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlFullScreen = new System.Windows.Forms.Panel();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.pnlPassWord = new System.Windows.Forms.Panel();
+            this.txbPassWord = new System.Windows.Forms.TextBox();
+            this.lblPassWord = new System.Windows.Forms.Label();
+            this.pnlUserName = new System.Windows.Forms.Panel();
+            this.txbUserName = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.pnlFullScreen.SuspendLayout();
+            this.pnlPassWord.SuspendLayout();
+            this.pnlUserName.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlFullScreen
+            // 
+            this.pnlFullScreen.Controls.Add(this.lblLogin);
+            this.pnlFullScreen.Controls.Add(this.btnExit);
+            this.pnlFullScreen.Controls.Add(this.btnLogin);
+            this.pnlFullScreen.Controls.Add(this.pnlPassWord);
+            this.pnlFullScreen.Controls.Add(this.pnlUserName);
+            this.pnlFullScreen.Location = new System.Drawing.Point(25, 10);
+            this.pnlFullScreen.Name = "pnlFullScreen";
+            this.pnlFullScreen.Size = new System.Drawing.Size(350, 180);
+            this.pnlFullScreen.TabIndex = 9;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.Location = new System.Drawing.Point(150, 10);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(50, 20);
+            this.lblLogin.TabIndex = 9;
+            this.lblLogin.Text = "LOG IN";
+            this.lblLogin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(250, 140);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 25);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "Thoát";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(165, 140);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 25);
+            this.btnLogin.TabIndex = 12;
+            this.btnLogin.Text = "Đăng nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // pnlPassWord
+            // 
+            this.pnlPassWord.Controls.Add(this.txbPassWord);
+            this.pnlPassWord.Controls.Add(this.lblPassWord);
+            this.pnlPassWord.Location = new System.Drawing.Point(25, 90);
+            this.pnlPassWord.Name = "pnlPassWord";
+            this.pnlPassWord.Size = new System.Drawing.Size(300, 40);
+            this.pnlPassWord.TabIndex = 11;
+            // 
+            // txbPassWord
+            // 
+            this.txbPassWord.Location = new System.Drawing.Point(120, 10);
+            this.txbPassWord.Name = "txbPassWord";
+            this.txbPassWord.Size = new System.Drawing.Size(160, 20);
+            this.txbPassWord.TabIndex = 5;
+            this.txbPassWord.UseSystemPasswordChar = true;
+            // 
+            // lblPassWord
+            // 
+            this.lblPassWord.Location = new System.Drawing.Point(20, 10);
+            this.lblPassWord.Name = "lblPassWord";
+            this.lblPassWord.Size = new System.Drawing.Size(90, 20);
+            this.lblPassWord.TabIndex = 4;
+            this.lblPassWord.Text = "Mật khẩu:";
+            this.lblPassWord.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pnlUserName
+            // 
+            this.pnlUserName.Controls.Add(this.txbUserName);
+            this.pnlUserName.Controls.Add(this.lblUserName);
+            this.pnlUserName.Location = new System.Drawing.Point(25, 40);
+            this.pnlUserName.Name = "pnlUserName";
+            this.pnlUserName.Size = new System.Drawing.Size(300, 40);
+            this.pnlUserName.TabIndex = 10;
+            // 
+            // txbUserName
+            // 
+            this.txbUserName.Location = new System.Drawing.Point(120, 10);
+            this.txbUserName.Name = "txbUserName";
+            this.txbUserName.Size = new System.Drawing.Size(160, 20);
+            this.txbUserName.TabIndex = 2;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.Location = new System.Drawing.Point(20, 10);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(90, 20);
+            this.lblUserName.TabIndex = 1;
+            this.lblUserName.Text = "Tên đăng nhập:";
+            this.lblUserName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(400, 200);
+            this.Controls.Add(this.pnlFullScreen);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Màn hình đăng nhập";
+            this.pnlFullScreen.ResumeLayout(false);
+            this.pnlPassWord.ResumeLayout(false);
+            this.pnlPassWord.PerformLayout();
+            this.pnlUserName.ResumeLayout(false);
+            this.pnlUserName.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel pnlFullScreen;
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Panel pnlPassWord;
+        private System.Windows.Forms.TextBox txbPassWord;
+        private System.Windows.Forms.Label lblPassWord;
+        private System.Windows.Forms.Panel pnlUserName;
+        private System.Windows.Forms.TextBox txbUserName;
+        private System.Windows.Forms.Label lblUserName;
+    }
+}
+
---
+++ Winform/AppQuanLyQuanCafe/Login.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+
+namespace AppQuanLyQuanCafe
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection sqlConnection = null;
+
+        private string CalculateSHA1(string input)
+        {
+
+            // Convert the password string to byte array
+            byte[] inputBytes = Encoding.Unicode.GetBytes(input);
+
+            // Create a new instance of SHA1 algorithm
+            using (SHA1 sha1 = SHA1.Create())
+            {
+                // Compute the hash value of the password bytes
+                byte[] hashBytes = sha1.ComputeHash(inputBytes);
+
+                // Convert the byte array to a hexadecimal string
+                string hashedInput = BitConverter.ToString(hashBytes).Replace("-", "").ToUpper();
+
+                // Print the hashed password
+                return hashedInput;
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (sqlConnection == null)
+            {
+                sqlConnection = new SqlConnection(@"Data Source=HG;Initial Catalog=QLSV;Integrated Security=True");
+            }
+            if (sqlConnection.State == ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+
+            string username = txbUserName.Text.Trim(); // Thay thế bằng tên đăng nhập được cung cấp
+            string password = txbPassWord.Text.Trim(); // Thay thế bằng mật khẩu được cung cấp
+
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.CommandType = CommandType.Text;
+            string querySinhVien = "SELECT COUNT(*) FROM SINHVIEN WHERE TENDN = @Username AND MATKHAU = 0x" + CalculateSHA1(password);
+            SqlCommand commandSinhVien = new SqlCommand(querySinhVien, sqlConnection);
+            commandSinhVien.Parameters.AddWithValue("@Username", username);
+            int countSinhVien = (int)commandSinhVien.ExecuteScalar();
+
+            // Kiểm tra xem có tồn tại trong bảng NHANVIEN không
+            string queryNhanVien = "SELECT COUNT(*) FROM NHANVIEN WHERE TENDN = @Username AND MATKHAU = 0x" + CalculateSHA1(password);
+            SqlCommand commandNhanVien = new SqlCommand(queryNhanVien, sqlConnection);
+            commandNhanVien.Parameters.AddWithValue("@Username", username);
+            int countNhanVien = (int)commandNhanVien.ExecuteScalar();
+
+            if (countSinhVien > 0 || countNhanVien > 0)
+            {
+                MessageBox.Show("Đăng Nhập Thành Công");
+            }
+            else
+            {
+                MessageBox.Show("Tên đăng nhập và mật khẩu không hợp lệ");
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
